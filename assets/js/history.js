224c29ec26f9f0dc1e7dbd99e378541cd22f26cc
devjldp <joseluisdp297@gmail.com>
2023-10-21 12:22:28 +0100

    Add history js code

---
+++ assets/js/history.js
@@ -0,0 +1,10 @@
+const cards = document.querySelectorAll('.card');
+
+window.addEventListener('DOMContentLoaded', () => {
+  // Iterate through each 'card' and toggle the class on the "clicked" card.
+  for (let i in cards) {
+    cards[i].addEventListener('click', () => {
+      cards[i].classList.toggle('is-flipped')
+    })
+  }
+})
