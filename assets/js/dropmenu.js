822f25dca3c8326ac0340398c8c28caed0281bef
devjldp <joseluisdp297@gmail.com>
2023-10-21 13:37:55 +0100

    Fixed drop menu bug position

--- assets/js/dropmenu.js
+++ assets/js/dropmenu.js
@@ -1,6 +1,9 @@
 let btnBurguer = document.getElementById("btn-hamburguer");
 let navigation = document.getElementById("navigation");
+let title = document.getElementById("history-title");
+
 
 btnBurguer.addEventListener('click', () => {
   navigation.classList.toggle('active');
+  title.classList.toggle('mt-12vw')
 });
