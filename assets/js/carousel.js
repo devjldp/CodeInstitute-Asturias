f4d70e5ca74e60e7001e0ce59a085a019d1e2686
devjldp <joseluisdp297@gmail.com>
2023-10-21 14:41:12 +0100

    Fix title bug in gallery.html

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,28 +1,38 @@
-new Swiper('#swiper-2', {
-  centeredSlides: true,
-  spaceBetween: 10,
-  lazyLoading: true,
-  loop: true,
-  keyboard: {
-    enabled: true
-  },
-  autoplay: {
-    delay: 9000,
-  },
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
+window.addEventListener('DOMContentLoaded', () => {
+  new Swiper('#swiper-2', {
+    centeredSlides: true,
+    spaceBetween: 10,
+    lazyLoading: true,
+    loop: true,
+    keyboard: {
+      enabled: true
     },
-    769: {
-      slidesPerView: 1.5,
-    }
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'progressbar',
-  },
+    autoplay: {
+      delay: 9000,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      769: {
+        slidesPerView: 1.5,
+      }
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'progressbar',
+    },
+  })
+
+
+  let btnG = document.getElementById("btn-hamburguer");
+  let gTitle = document.getElementById("gallery-title")
+
+  btnG.addEventListener('click', () => {
+    gTitle.classList.toggle('mt-12vw');
+  });
 })
