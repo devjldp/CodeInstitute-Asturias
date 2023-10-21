cd5987c73ea22326cb28f9bcc4002d715cd68b9b
devjldp <joseluisdp297@gmail.com>
2023-10-21 15:58:43 +0100

    Add showRecipe funcionality in recipe.js

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -9,3 +9,22 @@ const downArrowH = document.getElementById('down-arrow-h');
 const upArrowH = document.getElementById('up-arrow-h');
 const downArrowFr = document.getElementById('down-arrow-fr');
 const upArrowFr = document.getElementById('up-arrow-fr');
+
+const showRecipe = (element1, element2, element3) => {
+  element1.classList.toggle('hide');
+  element2.classList.toggle('hide');
+  element3.classList.toggle('hide');
+}
+window.addEventListener("DOMContentLoaded", () => {
+  for (let button of buttons) {
+    button.addEventListener('click', () => {
+      if (button.value == 'fabada') {
+        showRecipe(fabada, upArrowF, downArrowF);
+      } else if (button.value == 'hake') {
+        showRecipe(hake, upArrowH, downArrowH);
+      } else if (button.value == 'frixuelos') {
+        showRecipe(frixuelos, upArrowFr, downArrowFr);
+      }
+    })
+  }
+})
