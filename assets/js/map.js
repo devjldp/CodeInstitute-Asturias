041c195d51c1419d4034a75db64c4928cfd59240
devjldp <joseluisdp297@gmail.com>
2023-10-22 11:24:56 +0100

    Add leflet.legend plugin

--- assets/js/map.js
+++ assets/js/map.js
@@ -71,89 +71,120 @@ const getData = async (url) => {
 
 window.addEventListener('DOMContentLoaded', async () => {
   // const current = await getGeoLocation();
+  try {
+    const data = await getData('./assets/json/data.json');
+    const restaurants = data.restaurants;
+    const beaches = data.beaches;
+    const museums = data.museums;
 
-  const data = await getData('./assets/json/data.json');
-  const restaurants = data.restaurants;
-  const beaches = data.beaches;
-  const museums = data.museums;
+    const position = await getGeoLocation();
+    // Create a Leaflet map centered on the user's position
+    let map = L.map('map').setView([position[0], position[1]], 18);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap'
+    }).addTo(map);
+    // Add a marker for the user's current position
+    L.marker([position[0], position[1]], {
+      icon: currentPositionIcon
+    }).addTo(map)
+    let group = []
+    console.log(Object.keys(restaurants).length)
+    for (let clave in restaurants) {
+      if (restaurants[clave]['web'] != '') {
 
-  const position = await getGeoLocation();
-  // Create a Leaflet map centered on the user's position
-  let map = L.map('map').setView([position[0], position[1]], 18);
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '© OpenStreetMap'
-  }).addTo(map);
-  // Add a marker for the user's current position
-  L.marker([position[0], position[1]], {
-    icon: currentPositionIcon
-  }).addTo(map)
-  let group = []
-  console.log(Object.keys(restaurants).length)
-  for (let clave in restaurants) {
-    if (restaurants[clave]['web'] != '') {
-
-      if (restaurants[clave]['stars'] == 2) {
-        console.log(restaurants[clave]['stars'])
-        restaurantIcon = L.icon({
-          iconUrl: './assets/icons/gourmet_2stars.png'
-        });
-      } else if (restaurants[clave]['stars'] == 1) {
-        restaurantIcon = L.icon({
-          iconUrl: './assets/icons/gourmet_1star.png'
-        });
-      } else if (restaurants[clave]['stars'] == 0) {
+        if (restaurants[clave]['stars'] == 2) {
+          console.log(restaurants[clave]['stars'])
+          restaurantIcon = L.icon({
+            iconUrl: './assets/icons/gourmet_2stars.png'
+          });
+        } else if (restaurants[clave]['stars'] == 1) {
+          restaurantIcon = L.icon({
+            iconUrl: './assets/icons/gourmet_1star.png'
+          });
+        } else if (restaurants[clave]['stars'] == 0) {
+          restaurantIcon = L.icon({
+            iconUrl: './assets/icons/gourmet_0star.png'
+          });
+        }
+        group.push(L.marker([restaurants[clave]['lat'], restaurants[clave]['lon']], {
+            icon: restaurantIcon
+          }).addTo(map)
+          .bindPopup(`${restaurants[clave]['name']} </br> ${restaurants[clave]['location']} </br> <a href="${restaurants[clave]['web']}" target="_blank">${restaurants[clave]['name']}</a>`))
+      } else {
         restaurantIcon = L.icon({
           iconUrl: './assets/icons/gourmet_0star.png'
         });
+        group.push(L.marker([restaurants[clave]['lat'], restaurants[clave]['lon']], {
+            icon: restaurantIcon
+          }).addTo(map)
+          .bindPopup(`${restaurants[clave]['name']} </br> ${restaurants[clave]['location']}`))
       }
-      group.push(L.marker([restaurants[clave]['lat'], restaurants[clave]['lon']], {
-          icon: restaurantIcon
-        }).addTo(map)
-        .bindPopup(`${restaurants[clave]['name']} </br> ${restaurants[clave]['location']} </br> <a href="${restaurants[clave]['web']}" target="_blank">${restaurants[clave]['name']}</a>`))
-    } else {
-      restaurantIcon = L.icon({
-        iconUrl: './assets/icons/gourmet_0star.png'
-      });
-      group.push(L.marker([restaurants[clave]['lat'], restaurants[clave]['lon']], {
-          icon: restaurantIcon
-        }).addTo(map)
-        .bindPopup(`${restaurants[clave]['name']} </br> ${restaurants[clave]['location']}`))
+
     }
+    console.log(group.length)
 
+    let restaurantLayer = L.layerGroup(group)
+    group = []
+    for (let clave in beaches) {
+      group.push(L.marker([beaches[clave]['lat'], beaches[clave]['lon']], {
+          icon: beachIcon
+        }).addTo(map)
+        .bindPopup(`${beaches[clave]['name']} </br> `))
+    }
+    let beachLayer = L.layerGroup(group)
+    group = []
+    for (let clave in museums) {
+      group.push(L.marker([museums[clave]['lat'], museums[clave]['lon']], {
+          icon: museumIcon
+        }).addTo(map)
+        .bindPopup(`${museums[clave]['name']} </br> `))
+    }
+    let museumLayer = L.layerGroup(group)
+
+    let overlayMaps = {
+      "Restaurnat": restaurantLayer,
+      "Beach": beachLayer,
+      "Museum": museumLayer
+    };
+
+    L.control.layers(overlayMaps).addTo(map);
+
+    L.control.Legend({
+      position: "bottomright",
+      collapsed: true,
+      column: 1,
+      legends: [{
+          label: "Restaurant",
+          type: "image",
+          url: "./assets/icons/gourmet_0star.png",
+          layers: [restaurantLayer],
+          inactive: false
+        },
+        {
+          label: "Beach",
+          type: "image",
+          url: "./assets/icons/beach_icon.png",
+          layers: beachLayer,
+          inactive: false
+        },
+        {
+          label: "Museum",
+          type: "image",
+          url: "./assets/icons/museum.png",
+          layers: museumLayer,
+          inactive: false
+        }
+      ]
+    }).addTo(map);
+
+    let center = document.getElementById('center')
+    center.addEventListener('click', () => {
+      map.setView([43.362014825626254, -5.850953691346048], 10);
+      // restaurantLayer.remove()
+    })
+  } catch (error) {
+    console.error('Error fetching data:', error);
   }
-  console.log(group.length)
-
-  let restaurantLayer = L.layerGroup(group)
-  group = []
-  for (let clave in beaches) {
-    group.push(L.marker([beaches[clave]['lat'], beaches[clave]['lon']], {
-        icon: beachIcon
-      }).addTo(map)
-      .bindPopup(`${beaches[clave]['name']} </br> `))
-  }
-  let beachLayer = L.layerGroup(group)
-  group = []
-  for (let clave in museums) {
-    group.push(L.marker([museums[clave]['lat'], museums[clave]['lon']], {
-        icon: museumIcon
-      }).addTo(map)
-      .bindPopup(`${museums[clave]['name']} </br> `))
-  }
-  let museumLayer = L.layerGroup(group)
-
-  let overlayMaps = {
-    "Restaurnat": restaurantLayer,
-    "Beach": beachLayer,
-    "Museum": museumLayer
-  };
-
-  let layerControl = L.control.layers(overlayMaps).addTo(map);
-
-  let center = document.getElementById('center')
-  center.addEventListener('click', () => {
-    map.setView([43.362014825626254, -5.850953691346048], 10);
-    // restaurantLayer.remove()
-  })
 
 })
